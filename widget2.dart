0be1e45c7bf9c8a766f0700c4f3943809721d344
hinagithub <matsuno.hinako@gmail.com>
2024-04-21 19:41:28 +0900

    整頓

--- widget2.dart
+++ widget2.dart
@@ -7,26 +7,7 @@ import 'package:flutter/material.dart';
 
 // エントリポイント
 void main() {
-//   basicWidgets();
   statelessWidget();
-}
-
-// 基本のウィジェット
-void basicWidgets() {
-  runApp(
-    // ウィジェット① 矩形の描画
-    Container(
-      color: Colors.pink,
-      // ウィジェット② 中央配置のレイアウト
-      child: const Center(
-        // ウィジェット③ テキスの描画
-        child: Text(
-          '基本のウィジェット',
-          textDirection: TextDirection.ltr,
-        ),
-      ),
-    ),
-  );
 }
 
 // 状態を持たないウィジェット(StatelessWidget)
